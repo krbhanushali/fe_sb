cbae488a7cf93ed011c7ee34b374cbdc05894b41
krbhanushali <krishnabhanushali123@mail.com>
2020-12-19 22:12:52 +0530

    a

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import mid from './img/Group 35699.svg';
+import './App.css';
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+      <div className="App">
+        <div className="Top">
+          <div className="t1"/>
+          <div className="t2"/>
+          <div className="t3"/>
+          <div className="t4"/>
+          <div className="t5"/>
+          <div className="t6"/>
+          <div className="t7"/>
+          <div className="t8"/>
+          <div className="t9"/>
+          <div className="t10"/>
+          <div className="t11"/>
+          <div className="t12"/>
+          <div className="t13"/>
+          <div className="t14"/>
+          <div className="Rocket">
+            <div className="Ellipse"/>
+          </div>
+        </div>
+
+        <div className="Mid1"/>
+        <div className= "Mid2"/>
+        <div className= "Mid_dot"/>
+        <div className="horiz_bl"/>
+        <div className="tb_1"/>
+        <div className= "tr_2"/>
+        <div className= "tb_2"/>
+        <div className= "box"/>
+        <div className= "box_in"/>
+        <div className= "box2"/>
+        <div className= "box2_in"/>
+        <div className= "box3"/>
+        <div className= "box4"/>
+        <div className= "box3_in"/>
+        <div className= "box4_in"/>
+        <div className= "box5"/>
+        <div className= "box5_in"/>
+        <div className= "box6"/>
+        <div className= "box6_in"/>
+        <div className= "box7"/>
+        <div className= "box7_in"/>
+        <div className= "box8"/>
+        <div className= "box8_in"/>
+        <div className= "box9"/>
+        <div className= "box9_in"/>
+        <div className= "box10"/>
+        <div className= "box10_in"/>
+        <div className= "box11"/>
+        <div className= "box11_in"/>
+        <div className= "box12"/>
+        <div className= "box12_in"/>
+        <div className= "box13"/>
+        <div className= "box13_in"/>
+
+        <div className="rect_2"/>
+        <div className="face_2"/>
+        <div className= "face"/>
+        <div className= "arrow"/>
+        <div className= "arrow1"/>
+
+        <div className= "dots2"/>
+        <div className= "dots3"/>
+        <div className= "tri"/>
+        <div className= "endm1"/>
+        <div className= "end1"/>
+        <div className= "plane"/>
+        <div className= "high2"/>
+        <div className= "high3"/>
+        <div className="cs1"/>
+        <div className="cs2"/>
+        <div className="cs3"/>
+
+        <div className="high4"/>
+
+        <div className="contact_1"/>
+        <div className="contact_2"/>
+        <div className="contact_3"/>
+        <div className="contact_3in"/>
+        <div className="text_end_1">
+          Lorem Finaldream rure dolor in reprehenderit in voluptate velit esse cillum dolore e uis nostrud exercitation isi ut aliquip ex ea commodo consequat.
+          </div>
+        <div className="text_end_2">
+          Web Development
+          Mobile Development
+          Cloud Technologies
+          UI/UX Desins
+        </div>
+        </div>
+
+  );
+}
+
+export default App;
